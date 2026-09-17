e93507ace4774f2285740f17fe71e475e6daa652
DDAY2301 <135416858+DDAY2301@users.noreply.github.com>
2026-09-17 15:00:45 +0200

    Wire private terminal login into public header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import PrivateTerminalAccess from "./PrivateTerminalAccess.jsx";
 import "./styles.css";
+import "./production.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <PrivateTerminalAccess />
   </React.StrictMode>,
 );
