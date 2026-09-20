46121f35354cc0e79943315be0600ae7a861068d
DDAY2301 <135416858+DDAY2301@users.noreply.github.com>
2026-09-20 16:12:01 +0200

    Mount multilingual language tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import LanguageTools from "./LanguageTools.jsx";
 import PrivateTerminalAccess from "./PrivateTerminalAccess.jsx";
 import "./styles.css";
 import "./production.css";
@@ -8,6 +9,7 @@ import "./production.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <LanguageTools />
     <PrivateTerminalAccess />
   </React.StrictMode>,
 );
